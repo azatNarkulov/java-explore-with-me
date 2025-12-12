6bf30cdcd21d367b1acb29a2245b638a79fc3be9
Naritab_ <rzekicc@gmail.com>
2025-12-13 01:26:38 +0600

    Add exception && CategoryServiceImpl, CompilationServiceImpl, UserServiceImpl

--- main-service/src/main/java/ru/practicum/ewm/service/CategoryService.java
+++ main-service/src/main/java/ru/practicum/ewm/service/CategoryService.java
@@ -2,18 +2,19 @@ package ru.practicum.ewm.service;
 
 import ru.practicum.ewm.dto.CategoryDto;
 import ru.practicum.ewm.dto.NewCategoryDto;
-import ru.practicum.ewm.model.Category;
 
 import java.util.List;
 
 public interface CategoryService {
 
+    // Admin
     CategoryDto create(NewCategoryDto dto);
 
     void delete(Long id);
 
-    CategoryDto update(Long id, Category dto);
+    CategoryDto update(Long id, CategoryDto dto);
 
+    // Public
     List<CategoryDto> getAll(int from, int size);
 
     CategoryDto getById(Long id);
--- main-service/src/main/java/ru/practicum/ewm/service/CompilationService.java
+++ main-service/src/main/java/ru/practicum/ewm/service/CompilationService.java
@@ -8,12 +8,14 @@ import java.util.List;
 
 public interface CompilationService {
 
+    // Admin
     CompilationDto create(NewCompilationDto dto);
 
     void delete(Long id);
 
     CompilationDto update(Long id, UpdateCompilationRequest dto);
 
+    // Public
     List<CompilationDto> getAll(Boolean pinned, int from, int size);
 
     CompilationDto getById(Long id);
