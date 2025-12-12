040183bd469099ec12f7b1ae4d5bebdd73b54242
Naritab_ <rzekicc@gmail.com>
2025-12-12 15:09:50 +0600

    Add repositories

--- main-service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
@@ -1,7 +1,28 @@
 package ru.practicum.ewm.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import ru.practicum.ewm.enums.EventState;
 import ru.practicum.ewm.model.Event;
 
-public interface EventRepository extends JpaRepository<Event, Long> {
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
+
+    Page<Event> findByInitiatorId(Long initiatorId, Pageable pageable);
+
+    Page<Event> findByCategoryId(Long categoryId, Pageable pageable);
+
+    Page<Event> findByEventDateBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    List<Event> findAllByIdIn(Collection<Long> ids);
+
+    Optional<Event> findByIdAndState(Long id, EventState state);
 }
--- main-service/src/main/java/ru/practicum/ewm/repository/UserRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/repository/UserRepository.java
@@ -1,7 +1,19 @@
 package ru.practicum.ewm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.model.User;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    List<User> findAllByIdIn(Set<Long> ids);
 }
